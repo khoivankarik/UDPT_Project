0d013dbd5505d3d0702b2aaa8e1a95fb9ccc1613
Văn Đình Khôi <63282078+khoivankarik@users.noreply.github.com>
2023-07-08 15:49:28 +0700

    User Register
    part 5 complete

--- stackprj/stackusers/views.py
+++ stackprj/stackusers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Successfully created for {username}! Login In Now')
+            return redirect('stackbase:home')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'stackusers/register.html', {'form': form})
