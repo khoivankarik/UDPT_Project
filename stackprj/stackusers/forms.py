f57764870d8987c63bcf794440f7fe79476ad668
thinhphuoc <thinhphuoc6996@gmail.com>
2023-07-22 16:47:48 +0700

    update password done
    update password done

--- stackprj/stackusers/forms.py
+++ stackprj/stackusers/forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.db import models
 from django.db.models import fields
 from .models import Profile
+from django.contrib.auth.hashers import make_password
+
+
 
 class UserRegisterForm(UserCreationForm):
     email = models.EmailField()
@@ -17,7 +20,16 @@ class UserUpdateForm(forms.ModelForm):
     
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['password', 'email']
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data['password']
+        if password:
+            user.password = make_password(password)
+        if commit:
+            user.save()
+        return user
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
